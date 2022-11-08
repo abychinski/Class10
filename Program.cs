80f3cbd15515bdbbb059a3f27b4d9dad68198ed1
Andrew Bychinski <aebychinski@live.com>
2022-11-07 20:23:26 -0600

    Work

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Class10.Models;
+﻿using Castle.Core.Internal;
+using Class10.Models;
 
 namespace Class10
 {
@@ -6,62 +7,139 @@ namespace Class10
     {
         static void Main(string[] args)
         {
-            // 1. List Posts for Blog #1
-            //using (var db = new BlogContext())
-            //{
-            //    var blog = db.Blogs.Where(x => x.BlogId == 1).FirstOrDefault();
-            //    // var blogsList = blog.ToList(); // convert to List from IQueryable
-
-            //    System.Console.WriteLine($"Posts for Blog {blog.Name}");
-
-            //    foreach (var post in blog.Posts)
-            //    {
-            //        System.Console.WriteLine($"\tPost {post.PostId} {post.Title}");
-            //    }
-            //}
-            // 2. Add Post to database
-            // System.Console.WriteLine("Enter your Post title");
-            // var postTitle = Console.ReadLine();
-
-            // var post = new Post();
-            // post.Title = postTitle;
-            // post.BlogId = 1;
-
-            // using (var db = new BlogContext())
-            // {
-            //     db.Posts.Add(post);
-            //     db.SaveChanges();
-            // }
-
-            // 3. Read Blogs from database
-            using (var db = new BlogContext())
+            var choice = "";
+            do
             {
-                Console.WriteLine("Here is the list of blogs");
-                
-                foreach (var b in db.Blogs)
+                Console.WriteLine("(1) Display Blogs  \n(2) Add Blog \n(3) Display Posts  \n(4) Add Post");
+                choice = Console.ReadLine().ToLower();
+
+                if (choice == "2")
                 {
-                    Console.WriteLine($"Blog: {b.BlogId}: {b.Name}");
+                    Console.WriteLine("Enter Blog name: ");
+                    var blogName = Console.ReadLine();
+                    if (string.IsNullOrEmpty(blogName))
+                    {
+                        Console.WriteLine("Blog name cannot be empty! Enter Blog name: ");
+                        blogName = Console.ReadLine();
+                    }
+
+                    var blog = new Blog();
+                    blog.Name = blogName;
+
+                    using (var context = new BlogContext())
+                    {
+                        context.Add(blog);
+                        context.SaveChanges();
+                    }
                 }
-            }
 
-            // 4. Add Blog to Database
-            //System.Console.WriteLine("Enter your Blog name");
-            //var blogName = Console.ReadLine();
+                else if (choice == "1")
+                {
+                    using (var context = new BlogContext())
+                    {
+                        Console.WriteLine("Here is the list of blogs");
+                        foreach (var b in context.Blogs)
+                        {
+                            Console.WriteLine($"Blog: {b.BlogId}) {b.Name}");
+                        }
+                    }
+                }
 
-            //// Create new Blog
-            //var blog = new Blog();
-            //blog.Name = blogName;
+                else if (choice == "4")
+                {
+                    var post = new Post();
+                    Console.WriteLine("Enter your Post title");
+                    var postTitle = Console.ReadLine();
+                    if (string.IsNullOrEmpty(postTitle))
+                    {
+                        Console.WriteLine("Post Title cannot be empty! Enter Post Title: ");
+                        postTitle = Console.ReadLine();
+                    }
 
-            //// Save blog object to database
-            //var db = new BlogContext();
-            //db.Blogs.Add(blog);
-            //db.SaveChanges();
+                    post.Title = postTitle;
 
-            //using (var db = new BlogContext())
-            //{
-            //    db.Add(blog);
-            //    db.SaveChanges();
-            //}
+                    Console.WriteLine("Enter Post Content: ");
+                    var postContent = Console.ReadLine();
+                    if (string.IsNullOrEmpty(postContent))
+                    {
+                        Console.WriteLine("Content cannot be empty! Enter Post Content: ");
+                        postContent = Console.ReadLine();
+                    }
+                    post.Content = postContent;
+
+                    using (var context = new BlogContext())
+                    {
+
+                        Console.WriteLine("Enter BlogId of Post: ");
+                    }
+                    var blId = Console.ReadLine();
+                    if (string.IsNullOrEmpty(blId))
+                    {
+                        Console.WriteLine("BlogId cannot be empty! Enter BlogId: ");
+                        blId = Console.ReadLine();
+                    }
+                    int blogId;
+                    while (!int.TryParse(blId, out blogId))
+                    {
+                        Console.WriteLine("BlogId must be a number! Enter valid BlogId: ");
+
+                    }
+                    post.BlogId = blogId;
+                    using (var context = new BlogContext())
+                    {
+                        var blog = context.Blogs.FirstOrDefault(x => x.BlogId == blogId);
+                        if (blog == null)
+                        {
+                            Console.WriteLine("Blog does not exist!");
+                        }
+                        else
+                        {
+                            context.Posts.Add(post);
+                            context.SaveChanges();
+                        }
+                    }
+                }
+
+                else if (choice == "3")
+                {
+
+                    Console.WriteLine("Which Blog number do you want to see posts?: ");
+                    var blId = Console.ReadLine();
+                    if (string.IsNullOrEmpty(blId))
+                    {
+                        Console.WriteLine("BlogId cannot be empty! Enter BlogId: ");
+                        blId = Console.ReadLine();
+                    }
+
+                    int blogId;
+                    while (!int.TryParse(blId, out blogId))
+                    {
+                        Console.WriteLine("BlogId must be a number! Enter valid BlogId: ");
+                    }
+                    using (var context = new BlogContext())
+                    {
+                        var blog = context.Blogs.FirstOrDefault(x => x.BlogId == blogId);
+                        if (blog == null)
+                        {
+                            Console.WriteLine("Blog does not exist!");
+
+                        }
+                        else if (blog.Posts.IsNullOrEmpty())
+                        {
+                            Console.WriteLine($"There are no posts in {blog.Name}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Posts for Blog: {blog.Name}");
+                            foreach (var post in blog.Posts)
+                            {
+                                Console.WriteLine($"\tPost: {post.PostId}) {post.Title}: {post.Content}");
+                            }
+                        }
+
+                    }
+                }
+            } while (choice != "x");
 
         }
     }
